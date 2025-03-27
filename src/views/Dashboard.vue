259ed1f57406a545c91a818d6767543ea77beb72
ryanlaverick <ryan.laverick03@hotmail.co.uk>
2025-03-27 23:12:46 +0000

    Adds tailwind config, basic colour palette

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,49 @@
 </script>
 
 <template>
-  <p>Dashboard</p>
+  <div class="flex">
+    <div class="h-16 w-16 bg-amaranth-lightest" />
+    <div class="h-16 w-16 bg-amaranth-light" />
+    <div class="h-16 w-16 bg-amaranth-base" />
+    <div class="h-16 w-16 bg-amaranth-dark" />
+    <div class="h-16 w-16 bg-amaranth-darkest" />
+  </div>
+
+  <div class="flex">
+    <div class="h-16 w-16 bg-aquamarine-lightest" />
+    <div class="h-16 w-16 bg-aquamarine-light" />
+    <div class="h-16 w-16 bg-aquamarine-base" />
+    <div class="h-16 w-16 bg-aquamarine-dark" />
+    <div class="h-16 w-16 bg-aquamarine-darkest" />
+  </div>
+
+
+  <div class="flex">
+    <div class="h-16 w-16 bg-space-lightest" />
+    <div class="h-16 w-16 bg-space-light" />
+    <div class="h-16 w-16 bg-space-base" />
+    <div class="h-16 w-16 bg-space-dark" />
+    <div class="h-16 w-16 bg-space-darkest" />
+  </div>
+
+
+  <div class="flex">
+    <div class="h-16 w-16 bg-thistle-lightest" />
+    <div class="h-16 w-16 bg-thistle-light" />
+    <div class="h-16 w-16 bg-thistle-base" />
+    <div class="h-16 w-16 bg-thistle-dark" />
+    <div class="h-16 w-16 bg-thistle-darkest" />
+  </div>
+
+
+  <div class="flex">
+    <div class="h-16 w-16 bg-sunglow-lightest" />
+    <div class="h-16 w-16 bg-sunglow-light" />
+    <div class="h-16 w-16 bg-sunglow-base" />
+    <div class="h-16 w-16 bg-sunglow-dark" />
+    <div class="h-16 w-16 bg-sunglow-darkest" />
+  </div>
+
 </template>
 
 <style scoped>
